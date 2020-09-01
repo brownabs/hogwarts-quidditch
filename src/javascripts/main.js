d218fb1e9b6d00bc6fe0792d197d34fdac20ccdf
Abbey Brown <abbeybrownsemail@gmail.com>
2020-09-01 18:06:03 -0500

    project set up

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,14 @@
+import studentData from '../data/studentData';
+import studentList from './components/studentList';
+import navBar from './components/navBar';
+import houseButtonGroup from './components/houseButtonGroup';
+
 import '../styles/main.scss';
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  navBar.loadNavBar();
+  houseButtonGroup.createHouseButtonGroup();
+  studentList.createStudentList(studentData.getStudents());
 };
 
 init();
